8f83a1e999eeeab5325932649d4a2875f3140941
liusanxiang <1084605775@qq.com>
2025-11-03 00:29:57 +0800

    srt version to v1.5.3

--- scripts.d/50-srt.sh
+++ scripts.d/50-srt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/Haivision/srt.git"
-SCRIPT_COMMIT="a26a7021a501e8c4bc17c75f3a109cca529cf262"
+SCRIPT_COMMIT="09f35c0f1743e23f514cb41444504a7faeacf89e"
 
 ffbuild_depends() {
     echo base
