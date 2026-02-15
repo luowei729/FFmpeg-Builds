9980a62c05b3a9e1260693642db1fee2d5194a5c
luowei729 <1084605775@qq.com>
2026-02-15 09:58:10 +0800

    update Dockerfiles for 4.4.3

--- variants/linux64-gpl-shared.sh
+++ variants/linux64-gpl-shared.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source "$(dirname "$BASH_SOURCE")"/linux-install-shared.sh
 source "$(dirname "$BASH_SOURCE")"/defaults-gpl-shared.sh
-FF_CONFIGURE="$FF_CONFIGURE --enable-alsa"
--- variants/linux64-lgpl-shared.sh
+++ variants/linux64-lgpl-shared.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source "$(dirname "$BASH_SOURCE")"/linux-install-shared.sh
 source "$(dirname "$BASH_SOURCE")"/defaults-lgpl-shared.sh
-FF_CONFIGURE="$FF_CONFIGURE --enable-alsa"
